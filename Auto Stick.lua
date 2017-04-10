75852fbbcddc0feee7260accf38f28e02eaa78f9
spiregor <spiregor25201@gmail.com>
2017-04-10 20:09:41 +0300

    First realese

---
+++ Auto Stick.lua
@@ -0,0 +1,38 @@
+--Author: spiregor
+local AutoStick = {}
+
+AutoStick.IsToggled = Menu.AddOption( {"Item Specific", "Auto Magic Stick/Magic Wand"}, "Enable","")
+AutoStick.PercentHPOption = Menu.AddOption({ "Item Specific", "Auto Magic Stick/Magic Wand"},"Percent HP", "", 5, 95, 5)
+
+
+function AutoStick.OnUpdate()
+    if not Menu.IsEnabled(AutoStick.IsToggled) then return true end
+	--Log.Write(Menu.GetValue(AutoStick.PercentHPOption))
+	AutoStick.Combo()
+end
+
+function AutoStick.Combo()
+	local myHero = Heroes.GetLocal()
+	local myHP = Entity.GetMaxHealth(myHero)
+    local myMP = NPC.GetMana(myHero)
+	if not myHero then return end
+	local MagicStick  = NPC.GetItem(myHero, "item_magic_stick", true)
+	local MagicWand  = NPC.GetItem(myHero, "item_magic_wand", true)
+	local ValidHP = math.floor(Menu.GetValue(AutoStick.PercentHPOption)*myHP / 100)
+	--Log.Write(ValidHP)
+	if not NPC.HasState(myHero, Enum.ModifierState.MODIFIER_STATE_INVISIBLE) and (MagicStick) and not NPC.HasModifier(myHero, "modifier_ice_blast") then
+		if Item.GetCurrentCharges(MagicStick)>=1 and Entity.GetHealth(myHero) <= ValidHP  then
+		  if Ability.IsCastable(MagicStick, 0) then Ability.CastNoTarget(MagicStick) return end
+		end
+	end
+
+	if not NPC.HasState(myHero, Enum.ModifierState.MODIFIER_STATE_INVISIBLE) and (MagicWand) and not NPC.HasModifier(myHero, "modifier_ice_blast") then	
+		if Item.GetCurrentCharges(MagicWand)>=1 and Entity.GetHealth(myHero) <= ValidHP then
+		  if Ability.IsCastable(MagicWand, 0) then Ability.CastNoTarget(MagicWand) return end
+		end
+	end
+    --Log.Write("Charge Stick"..":"..Item.GetCurrentCharges(MagicStick))
+	--Log.Write("Charge Stick"..":"..Item.GetCurrentCharges(MagicStick).."   ".."Charge Wand"..":"..Item.GetCurrentCharges(MagicWand))
+end
+
+return AutoStick
